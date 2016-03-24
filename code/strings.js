3a4e7326647068e902efab80d6058f1c7d84babf
dphaener <dphaener@gmail.com>
2016-03-24 09:40:49 -0700

    Update readme and fix code

--- code/strings.js
+++ code/strings.js
@@ -1,10 +1,10 @@
 export function strip(pieces, ...value) {
-  var str = ''
+  let str = '';
 
   pieces.forEach((piece, index) => {
-    let val = value[index] || ''
-    str = str + piece + val
-  })
+    let val = value[index] || '';
+    str += piece + val;
+  });
 
-  return str.replace(/^\s*/gm, '')
+  return str.replace(/^\s*/gm, '');
 }
